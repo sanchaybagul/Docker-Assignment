79b6b6e76c8f172f4977a36981741659c37213ed
Sanchay Bagul <sanchay.bagul@gmail.com>
2022-01-07 21:32:46 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,7 @@
+create database pucsdStudents ;
+use pucsdStudents;
+
+create table studentData(name varchar(20),rollno int,address varchar(30),mob int,PAN int);
+
+insert into studentData(name,rollno,address,mob,PAN)values('sanchay',1,'azxc',123456,1233445);
+insert into studentData(name,rollno,address,mob,PAN)values('akshay',2,'bzxc',123456,1345343);
